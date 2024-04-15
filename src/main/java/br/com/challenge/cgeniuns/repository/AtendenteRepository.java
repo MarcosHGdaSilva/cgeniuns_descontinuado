8fc86a91232df88359090369315a6a6c8c684e23
MarcosHGdaSilva <henriquegarrido1998@gmail.com>
2024-04-15 11:20:42 -0300

    finalizado

--- src/main/java/br/com/challenge/cgeniuns/repository/AtendenteRepository.java
+++ src/main/java/br/com/challenge/cgeniuns/repository/AtendenteRepository.java
@@ -1,16 +1,19 @@
 package br.com.challenge.cgeniuns.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.challenge.cgeniuns.model.Atendente;
 
 public interface AtendenteRepository extends JpaRepository<Atendente, Long> {
-
     @Query("SELECT atendente FROM Atendente atendente WHERE atendente.cpf_atendente = :cpf_atendente")
     Atendente findByCpf_atendente(@Param("cpf_atendente") String cpf_atendente);
-    @Query("SELECT atendente FROM Atendente atendente WHERE atendente.cpf_atendente = :cpf_atendente")
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Atendente atendente WHERE atendente.cpf_atendente = :cpf_atendente")
     void deleteByCpf_atendente(@Param("cpf_atendente") String cpf_atendente);
     
 }
--- src/main/java/br/com/challenge/cgeniuns/repository/ClienteRepository.java
+++ src/main/java/br/com/challenge/cgeniuns/repository/ClienteRepository.java
@@ -1,15 +1,19 @@
 package br.com.challenge.cgeniuns.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.challenge.cgeniuns.model.Cliente;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
     @Query("SELECT cliente FROM Cliente cliente WHERE cliente.cpf_cliente = :cpf_cliente")
     Cliente findByCpf_cliente(@Param("cpf_cliente") String cpf_cliente);
-    @Query("SELECT cliente FROM Cliente cliente WHERE cliente.cpf_cliente = :cpf_cliente")
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Cliente cliente WHERE cliente.cpf_cliente = :cpf_cliente")
     void deleteByCpf_cliente(@Param("cpf_cliente") String cpf_cliente);
 
 }
