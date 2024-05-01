53d7b872703363caf51299cefff20cda80b1bc7a
Marcos Henrique <40180008+MarcosHGdaSilva@users.noreply.github.com>
2024-05-01 21:09:25 +0000

    adicionando Seeders

--- src/main/java/br/com/challenge/cgeniuns/repository/AtendenteRepository.java
+++ src/main/java/br/com/challenge/cgeniuns/repository/AtendenteRepository.java
@@ -1,10 +1,6 @@
 package br.com.challenge.cgeniuns.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.transaction.annotation.Transactional;
 
 import br.com.challenge.cgeniuns.model.Atendente;
 
